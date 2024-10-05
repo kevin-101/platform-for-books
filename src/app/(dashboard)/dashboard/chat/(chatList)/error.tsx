1af446cb2395a7c366e0937881fd3af69ef780df
Kevin <kevinknoel7@gmail.com>
2024-10-05 08:06:14 +0530

    modified the message and removed the try again button in all error files

--- src/app/(dashboard)/dashboard/chat/(chatList)/error.tsx
+++ src/app/(dashboard)/dashboard/chat/(chatList)/error.tsx
@@ -1,14 +1,11 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { useEffect } from "react";
 
 export default function Error({
   error,
-  reset,
 }: {
   error: Error & { digest?: string };
-  reset: () => void;
 }) {
   useEffect(() => {
     console.error(error);
@@ -18,10 +15,9 @@ export default function Error({
     <div className="flex flex-col gap-4 h-full items-center justify-center">
       <h1 className="text-3xl font-bold">Something went wrong!</h1>
       <p className="text-lg font-medium text-center">
-        Refresh or navigate to a different route if error persists
+        Please refresh or navigate to a different route
       </p>
       <p className="text-lg font-medium text-center">{error.digest}</p>
-      <Button onClick={() => reset()}>Try Again</Button>
     </div>
   );
 }
--- src/app/(dashboard)/dashboard/search/error.tsx
+++ src/app/(dashboard)/dashboard/search/error.tsx
@@ -1,14 +1,11 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { useEffect } from "react";
 
 export default function error({
   error,
-  reset,
 }: {
   error: Error & { digest?: string };
-  reset: () => void;
 }) {
   useEffect(() => {
     console.error(error.message);
@@ -18,10 +15,9 @@ export default function error({
     <div className="flex flex-col items-center justify-center h-full gap-4">
       <h1 className="text-3xl font-bold">Something went wrong!</h1>
       <p className="text-lg font-medium text-center">
-        Refresh or navigate to a different route if error persists
+        Please refresh or navigate to a different route
       </p>
       <p className="text-lg font-medium text-center">{error.digest}</p>
-      <Button onClick={() => reset()}>Try Again</Button>
     </div>
   );
 }
