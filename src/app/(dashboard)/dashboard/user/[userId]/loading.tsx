1473daa3c68e3ca357a930abe6788e37877278ea
Kevin <kevinknoel7@gmail.com>
2024-10-09 08:25:00 +0530

    fixed loading files for profile and user pages

--- src/app/(dashboard)/dashboard/user/[userId]/loading.tsx
+++ src/app/(dashboard)/dashboard/user/[userId]/loading.tsx
@@ -4,7 +4,7 @@ export default function UserLoading() {
   return (
     <div className="flex flex-col gap-10 items-center pt-4 md:pt-8">
       <div className="flex items-center gap-4 md:gap-8 w-full px-4">
-        <Skeleton className="relative size-20 md:size-60 rounded-full" />
+        <Skeleton className="relative size-[5.5rem] md:size-[15.5rem] rounded-full" />
         <div className="flex flex-col flex-1 gap-2">
           <Skeleton className="w-[40%] h-6 md:h-8" />
           <Skeleton className="w-[70%] h-6 md:h-8" />
@@ -12,7 +12,7 @@ export default function UserLoading() {
       </div>
 
       <div className="flex flex-col gap-4 w-full">
-        <h2 className="text-lg md:text-xl font-medium text-center md:text-start w-full px-4">
+        <h2 className="text-lg md:text-xl font-medium text-center w-full px-4">
           Shared books
         </h2>
 
