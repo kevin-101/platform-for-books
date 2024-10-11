8055bf0975fc9be6a6821637922796cfea0ec16e
Kevin <kevinknoel7@gmail.com>
2024-10-11 07:56:31 +0530

    fixed loading file for user page

--- src/app/(dashboard)/dashboard/user/[userId]/loading.tsx
+++ src/app/(dashboard)/dashboard/user/[userId]/loading.tsx
@@ -3,11 +3,24 @@ import { Skeleton } from "@/components/ui/skeleton";
 export default function UserLoading() {
   return (
     <div className="flex flex-col gap-10 items-center pt-4 md:pt-8">
-      <div className="flex items-center gap-4 md:gap-8 w-full px-4">
-        <Skeleton className="relative size-[5.5rem] md:size-[15.5rem] rounded-full" />
-        <div className="flex flex-col flex-1 gap-2">
-          <Skeleton className="w-[40%] h-6 md:h-8" />
-          <Skeleton className="w-[70%] h-6 md:h-8" />
+      <div className="flex flex-col gap-4 w-full">
+        <div className="flex items-center gap-4 md:gap-8 w-full px-4">
+          <Skeleton className="relative size-[5.5rem] md:size-[15.5rem] rounded-full" />
+
+          <div className="flex flex-col flex-1 gap-2">
+            <Skeleton className="w-[40%] h-6 md:h-8" />
+            <Skeleton className="w-[70%] h-6 md:h-8" />
+          </div>
+
+          <div className="hidden md:flex flex-col items-center gap-2 w-1/4">
+            <Skeleton className="h-10 w-full" />
+            <Skeleton className="h-10 w-full" />
+          </div>
+        </div>
+
+        <div className="flex md:hidden gap-2 w-full justify-center px-4 *:w-1/2">
+          <Skeleton className="h-10" />
+          <Skeleton className="h-10" />
         </div>
       </div>
 
