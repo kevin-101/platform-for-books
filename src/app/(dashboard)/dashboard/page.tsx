655440c9cc7470a10fd1b21f8f600d464c157c4c
Kevin <kevinknoel7@gmail.com>
2024-10-16 08:12:42 +0530

    added friend books card to dashboard with dummy data

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,12 +1,19 @@
+import FriendBooks from "@/components/FriendBooks";
 import TopBooks from "@/components/TopBooks";
 
 export default function Page() {
   return (
-    <div className="grid md:grid-cols-3 px-4 py-6">
+    <div className="grid md:grid-cols-3 gap-4 px-4 py-6">
       {/* Top Searched Books */}
       <div className="flex flex-col gap-8 md:col-span-1 min-h-20 border border-border rounded-md p-4">
         <h2 className="text-xl font-medium">Top searched books</h2>
         <TopBooks />
+      </div>
+
+      {/* Books shared by friends */}
+      <div className="flex flex-col gap-8 md:col-span-2 min-h-20 border border-border rounded-md p-4">
+        <h2 className="text-xl font-medium">Books shared by friends</h2>
+        <FriendBooks />
       </div>
     </div>
   );
---
+++ src/components/FriendBooks.tsx
@@ -0,0 +1,31 @@
+import { UserIcon } from "lucide-react";
+import Link from "next/link";
+
+export default function FriendBooks() {
+  return (
+    <ul className="flex flex-col gap-4">
+      {[...Array(4)].map((_, i) => {
+        return (
+          <li
+            key={i}
+            className="border border-border rounded-md overflow-hidden bg-accent/65 hover:bg-accent/80 transition-colors"
+          >
+            <Link
+              href={`/dashboard/friends`}
+              className="flex gap-2 justify-between items-center p-2"
+            >
+              <div className="relative size-16 flex items-center justify-center rounded-full bg-accent">
+                <UserIcon className="size-5" />
+              </div>
+
+              <div className="flex flex-col gap-1 flex-1">
+                <h3 className="text-lg font-medium">Friend Name</h3>
+                <p>Book Title</p>
+              </div>
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
