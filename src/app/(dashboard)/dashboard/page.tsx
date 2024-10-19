68acfc9d685f812a43ec62a76383c547c91bd4f7
Kevin <kevinknoel7@gmail.com>
2024-10-19 08:24:12 +0530

    removed some redundant classnames in dashboard grid layout

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -5,13 +5,13 @@ export default function Page() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-3 gap-4 px-4 py-6">
       {/* Top Searched Books */}
-      <div className="flex flex-col gap-8 col-span-1 border border-border rounded-md p-4">
+      <div className="flex flex-col gap-8 border border-border rounded-md p-4">
         <h2 className="text-xl font-medium">Top searched books</h2>
         <TopBooks />
       </div>
 
       {/* Books shared by friends */}
-      <div className="flex flex-col gap-8 col-span-1 xl:col-span-2 border border-border rounded-md p-4">
+      <div className="flex flex-col gap-8 xl:col-span-2 border border-border rounded-md p-4">
         <h2 className="text-xl font-medium">Books shared by friends</h2>
         <FriendBooks />
       </div>
